ea3d05d0fa570d8f2dfc0306eeef1a593b085ec3
lynxx007 <luthfirizky7@gmail.com>
2023-12-04 10:48:30 +0700

    created Spinner

---
+++ src/client/components/Spinner.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import CircularProgress from "@mui/material/CircularProgress";
+
+const Spinner = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <CircularProgress />
+    </Box>
+  );
+};
+export default Spinner;
