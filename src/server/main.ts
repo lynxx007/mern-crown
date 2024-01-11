f4e71604b8fb18f4aedcc027f2f72c5b661e35c1
lynxx007 <luthfirizky7@gmail.com>
2024-01-11 14:06:05 +0700

    setup main.ts

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,12 +1,52 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import connectDb from "./config/connectDb";
+import { errorHandler } from "./middlewares/errorCheck";
+import HomeRoute from "../server/routes/home";
+import ShopRoute from "../server/routes/shop";
+import AuthRoute from "../server/routes/auth";
+import ProductRoute from "../server/routes/product";
+import cookieParser from "cookie-parser";
+import mongoSanitize from "express-mongo-sanitize";
+import { createClient } from "redis";
+import UserRoute from "../server/routes/user";
+import PaymentRoute from "../server/routes/payment";
+import "dotenv/config";
+
+connectDb();
+
+export const client = createClient({
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+  },
+});
+
+client.on("connect", () => {
+  console.log("Redis client connected to the server");
+});
+
+client.connect();
 
 const app = express();
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(mongoSanitize());
 
 app.get("/hello", (_, res) => {
   res.send("Hello Vite + React + TypeScript!");
 });
 
+app.use("/api/v1/home", HomeRoute);
+app.use("/api/v1/shop", ShopRoute);
+app.use("/api/v1/auth", AuthRoute);
+app.use("/api/v1/user", UserRoute);
+app.use("/api/v1/product", ProductRoute);
+app.use("/api/v1/payment", PaymentRoute);
+
+app.use(errorHandler);
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000...")
 );
