438786effb52b05c5b3103300b4e23e0e51c9122
lynxx007 <luthfirizky7@gmail.com>
2024-01-12 09:55:27 +0700

    setup Providers

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import { ThemeProvider } from "@mui/material/styles";
 import App from "./App";
+import theme from "./customTheme";
+import { Provider } from "react-redux";
+import { store, persistor } from "./app/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
