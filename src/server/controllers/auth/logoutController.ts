4d19917ac936bc8534f2754a274f149c12a225b1
lynxx007 <luthfirizky7@gmail.com>
2024-01-14 12:41:51 +0700

    created logoutController

---
+++ src/server/controllers/auth/logoutController.ts
@@ -0,0 +1,37 @@
+import expressAsyncHandler from "express-async-handler";
+import User from "../../models/UserModel";
+import "dotenv/config";
+
+const logout = expressAsyncHandler(async (req, res) => {
+  const cookies = req.cookies;
+
+  if (!cookies.jwt) {
+    res.status(204);
+    throw new Error("No token found");
+  }
+
+  const existUser = await User.findOne({ refreshToken: cookies.jwt });
+
+  if (!existUser) {
+    res.status(204).clearCookie("jwt", {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    });
+    throw new Error("No user found");
+  }
+
+  existUser.refreshToken = existUser.refreshToken.filter(
+    (token) => token !== cookies.jwt
+  );
+  await existUser.save();
+
+  res.clearCookie("jwt", {
+    sameSite: "none",
+    secure: true,
+    httpOnly: true,
+  });
+  res.status(200).json({ success: true, msg: `Logged out` });
+});
+
+export default logout;
