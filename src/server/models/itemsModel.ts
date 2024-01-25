ba2f1b8a4a7f636e3a35f663fd1af2ab5e1d5c0b
lynxx007 <luthfirizky7@gmail.com>
2024-01-25 09:31:36 +0700

    added itemsModel

---
+++ src/server/models/itemsModel.ts
@@ -0,0 +1,38 @@
+import mongoose,{Document,Schema,Model} from "mongoose";
+
+
+interface Item {
+    id:number,
+    name:string,
+    imageUrl:string,
+    price:number
+}
+
+interface Category {
+    title:string,
+    items: Item[]
+}
+
+interface ShopDataDocument extends Document{
+    categories: Category[]
+}
+
+interface ShopDataModel extends Model<ShopDataDocument>{}
+
+const ItemSchema = new Schema<Item>({
+    id:Number,
+    name:String,
+    imageUrl:String,
+    price:Number
+})
+
+const CategorySchema = new Schema<Category>({
+    title:String,
+    items:[ItemSchema]
+})
+
+const ShopData = mongoose.model<ShopDataDocument,ShopDataModel>("ShopData",new Schema({
+    categories:[CategorySchema]
+}))
+
+export default ShopData;
