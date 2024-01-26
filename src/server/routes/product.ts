94fe03eed76518b19b0bb24461fc8905d3d6e9b2
lynxx007 <luthfirizky7@gmail.com>
2024-01-26 10:42:30 +0700

    added getAllProducts route

--- src/server/routes/product.ts
+++ src/server/routes/product.ts
@@ -4,6 +4,7 @@ import { upload } from "../config/multer";
 import roleCheck from "../middlewares/roleCheck";
 import addProduct from "../controllers/product/addProduct";
 import categoriesPreview from "../controllers/categoriesPreview/categoriesPreview";
+import getAllProducts from "../controllers/product/getAllProducts";
 
 const router = express.Router();
 
@@ -16,5 +17,7 @@ router.post(
 );
 
 router.get("/searchProducts", checkAuth, categoriesPreview);
+
+router.get("/getAllProducts", checkAuth, getAllProducts);
 
 export default router;
