39bf315b1b8351493432fd0542e7b7244c4249f2
lynxx007 <luthfirizky7@gmail.com>
2024-01-27 10:34:39 +0700

    created route for /getAmountPaid

--- src/server/routes/payment.ts
+++ src/server/routes/payment.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import createPaymentIntent from "../controllers/payment/createPaymentIntent";
 import checkAuth from "../middlewares/checkAuth";
+import getTotalAmountPaid from "../controllers/payment/getTotalAmountPaid";
 
 const router = express.Router();
 
 router.post("/create-payment-intent", checkAuth, createPaymentIntent);
+
+router.get("/getAmountPaid", checkAuth, getTotalAmountPaid);
 
 export default router;
