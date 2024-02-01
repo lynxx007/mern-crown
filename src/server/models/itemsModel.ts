da9a3558036f1d1e232f25fd458cccf698f1f09a
lynxx007 <luthfirizky7@gmail.com>
2024-02-01 10:25:08 +0700

    .

--- src/server/models/itemsModel.ts
+++ src/server/models/itemsModel.ts
@@ -1,38 +1,40 @@
-import mongoose,{Document,Schema,Model} from "mongoose";
+import mongoose, { Document, Schema, Model } from "mongoose";
 
-
-interface Item {
-    id:number,
-    name:string,
-    imageUrl:string,
-    price:number
+export interface Item {
+  id: number;
+  name: string;
+  imageUrl: string;
+  price: number;
 }
 
 interface Category {
-    title:string,
-    items: Item[]
+  title: string;
+  items: Item[];
 }
 
-interface ShopDataDocument extends Document{
-    categories: Category[]
+interface ShopDataDocument extends Document {
+  categories: Category[];
 }
 
-interface ShopDataModel extends Model<ShopDataDocument>{}
+interface ShopDataModel extends Model<ShopDataDocument> {}
 
 const ItemSchema = new Schema<Item>({
-    id:Number,
-    name:String,
-    imageUrl:String,
-    price:Number
-})
+  id: Number,
+  name: String,
+  imageUrl: String,
+  price: Number,
+});
 
 const CategorySchema = new Schema<Category>({
-    title:String,
-    items:[ItemSchema]
-})
+  title: String,
+  items: [ItemSchema],
+});
 
-const ShopData = mongoose.model<ShopDataDocument,ShopDataModel>("ShopData",new Schema({
-    categories:[CategorySchema]
-}))
+const ShopData = mongoose.model<ShopDataDocument, ShopDataModel>(
+  "ShopData",
+  new Schema({
+    categories: [CategorySchema],
+  })
+);
 
 export default ShopData;
